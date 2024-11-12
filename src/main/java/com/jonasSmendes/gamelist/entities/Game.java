9db07f8cff05d8a8a1285a010ff1522a8826fe62
jonas mendes <jonas.jsm2903@gmail.com>
2024-11-12 14:14:05 -0300

    properties, game, ORM

---
+++ src/main/java/com/jonasSmendes/gamelist/entities/Game.java
@@ -0,0 +1,44 @@
+package com.jonasSmendes.gamelist.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_game")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platform;
+    private double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Game game)) return false;
+        return Objects.equals(getId(), game.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
